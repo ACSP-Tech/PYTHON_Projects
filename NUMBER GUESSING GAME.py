cfb84e5854a54e360f6542b8c69055df2b9588a1
Precious Chioma Aguluka <acspworld@gmail.com>
2023-11-06 16:54:09 +0000

    Update NUMBER GUESSING GAME.py
    This game allows you to guess a number from 1 to 10. giving only 3 trials.

--- NUMBER GUESSING GAME.py
+++ NUMBER GUESSING GAME.py
@@ -1,25 +1,16 @@
-print("\n\nNUMBER GUESSING GAME...")
 import random
-num = random.randint(1,10)
-guess = int(input("There are only 3 chances. \nGuess a number from 1 to 10: "))
-if guess < num:
-    print("your guess was smaller than the actual number!")
-elif guess > num:
-    print("Your guess was greater than the actual number!")
 index = 0
-while index <= 1:
+num = random.randint(1,10)
+while index <= 2:
+    var = index + 1 
+    guess = int(input("There are only 3 chances. Guess a number from 1 to 10: "))
     if guess == num:
         print("You guessed it correctly!")
         break
-    print("your ", index + 2, " chance")
-    guess = int(input("Try again, Guess a number from 1 to 10: "))
-    if index == 1 and guess != num:
+    if index == 2 and guess != num:
         print("wrong answer! the number is: ", num, "\nplay again! your 3 chances has been used up")
     elif guess < num:
-        print("your guess was smaller than the actual number!")
+        print("your guess was smaller than the actual number!. This is your", var, "attempt" )
     elif guess > num:
-        print("Your guess was greater than the actual number!")
-    elif guess == num:
-        print("You guessed it correctly!")
-        break
+        print("Your guess was greater than the actual number!. This is your", var, "attempt")
     index += 1
